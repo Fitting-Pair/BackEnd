827455763591a99e7378a0640384ea3a45e6dd69
chaerish <ccyy8432@naver.com>
2024-05-27 12:20:31 +0900

    feat: 초기 패키지 세팅

---
+++ FittingPair/src/main/java/smu/FittingPair/FittingPairApplication.java
@@ -0,0 +1,14 @@
+package smu.FittingPair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittingPairApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FittingPairApplication.class, args);
+	}
+
+}
---
+++ FittingPair/src/test/java/smu/FittingPair/FittingPairApplicationTests.java
@@ -0,0 +1,13 @@
+package smu.FittingPair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittingPairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
