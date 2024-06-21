d2cf59ade7450e6a7023f24321cec4aa1370bb1d
kimsj0970 <kimsj0970@naver.com>
2024-06-21 12:56:55 +0900

    feat : UserImg Entity add

--- FittingPair/src/main/java/smu/FittingPair/model/UserImg.java
+++ FittingPair/src/main/java/smu/FittingPair/model/UserImg.java
@@ -1,2 +1,20 @@
-package smu.FittingPair.model;public class UserImg {
+package smu.FittingPair.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter @Setter @Entity
+public class UserImg {
+
+    @Id @GeneratedValue
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    private String image_rul;
+    private String created_at; // 나중에 타입 변경 예정
+
 }
