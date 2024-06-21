884bc8d81669cb090c98d104346c675b365d4f91
kimsj0970 <kimsj0970@naver.com>
2024-06-21 12:55:19 +0900

    feat : UserBodyType Entity add

--- FittingPair/src/main/java/smu/FittingPair/model/UserBodyType.java
+++ FittingPair/src/main/java/smu/FittingPair/model/UserBodyType.java
@@ -1,2 +1,18 @@
-package smu.FittingPair.model;public class UserBodyType {
+package smu.FittingPair.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @Entity
+@RequiredArgsConstructor
+public class UserBodyType {
+    @Id @GeneratedValue
+    @Column(name = "userbodytype_id")
+    private Long userbodytype_id;
+
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_idg")
+    private Users users;
+
 }
