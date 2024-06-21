ba4f5485c1b8d699c1e8a1c7819710110f1ac148
kimsj0970 <kimsj0970@naver.com>
2024-06-21 12:50:03 +0900

    feat: BodySize Entitiy add

--- FittingPair/src/main/java/smu/FittingPair/model/BodySize.java
+++ FittingPair/src/main/java/smu/FittingPair/model/BodySize.java
@@ -1,2 +1,22 @@
-package smu.FittingPair.model;public class BodySize {
+package smu.FittingPair.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity @Getter @Setter @RequiredArgsConstructor
+public class BodySize {
+    @Id @GeneratedValue
+    @Column(name = "bodysize_id")
+    private Long bodysize_id;
+
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_id")
+    private Users users;
+
+    private Long chest_size;
+    private Long hip_size;
+    private Long shoulder_size;
+    private Long waist_size;
+    private Long height_size;
 }
