40677e97629698dfc7fd232753839eac139332e6
kimsj0970 <kimsj0970@naver.com>
2024-06-21 12:54:16 +0900

    feat : Result Entity add

---
+++ FittingPair/src/main/java/smu/FittingPair/model/Result.java
@@ -0,0 +1,26 @@
+package smu.FittingPair.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.apache.logging.log4j.util.Lazy;
+
+@RequiredArgsConstructor
+@Entity @Getter @Setter
+public class Result {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "result_id")
+    private Long result_id;
+
+
+    @OneToOne @JoinColumn(name = "userbodytype_id")
+    private UserBodyType userBodyType_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mypage_id")
+    private MyPage mypage;
+
+
+}
