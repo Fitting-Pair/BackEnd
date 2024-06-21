45975aece025ea29f1bdb738d7eb0e87f9e5224d
kimsj0970 <kimsj0970@naver.com>
2024-06-21 12:56:21 +0900

    feat : UserClothers Entity add

--- FittingPair/src/main/java/smu/FittingPair/model/UserClothers.java
+++ FittingPair/src/main/java/smu/FittingPair/model/UserClothers.java
@@ -1,2 +1,22 @@
-package smu.FittingPair.model;public class userClothers {
+package smu.FittingPair.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity @Getter @Setter @RequiredArgsConstructor
+public class UserClothers {
+    @Id @GeneratedValue
+    @Column(name = "userclothes_id")
+    private Long userclothes_id;
+
+    @ManyToOne @JoinColumn(name = "user_id")
+    private Users users;
+
+    @ManyToOne @JoinColumn(name = "result_id")
+    private Result results;
 }
