94bb98531823142d04e33a3a6600058b1ba5fde7
chaerish <ccyy8432@naver.com>
2024-06-29 23:06:33 +0900

    rename: LoginService -> AuthService change class name

--- FittingPair/src/main/java/smu/FittingPair/controller/LoginController.java
+++ FittingPair/src/main/java/smu/FittingPair/controller/LoginController.java
@@ -3,27 +3,30 @@ package smu.FittingPair.controller;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import smu.FittingPair.Service.LoginService;
+import smu.FittingPair.Service.AuthService;
 import smu.FittingPair.Service.SignUpService;
 import smu.FittingPair.config.response.BaseResponse;
 import smu.FittingPair.dto.LoginRequestDto;
-import smu.FittingPair.dto.SignUpRequestDto;
-import smu.FittingPair.model.CustomUserDetails;
 
 
 @RestController
 @RequiredArgsConstructor
 public class LoginController {
-    private final LoginService loginService;
+    private final AuthService authService;
     private final SignUpService signUpService;
     @PostMapping("/login")
     //todo: 전화번호 하나 뿐인데 굳이 dto로 감싸야 하는지..?
     public BaseResponse<?> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response){
-        return BaseResponse.ok(loginService.login(loginRequestDto,response));
+        return BaseResponse.ok(authService.login(loginRequestDto,response));
     }
     @GetMapping("/users")
     public BaseResponse<?> getUserInfo(){
-        return BaseResponse.ok(signUpService.getUserInfo(loginService.currentUserId()));
+        return BaseResponse.ok(signUpService.getUserInfo(authService.currentUserId()));
+    }
+    @PostMapping
+    public BaseResponse<?> logout(){
+
+        return BaseResponse.ok();
     }
 
 }
