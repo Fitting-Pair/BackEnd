16c17f6256c117eb409da752870af9147c2c7d5a
chaerish <ccyy8432@naver.com>
2024-07-30 18:50:20 +0900

    fix: reissue-token 관련 로직 수정

--- FittingPair/src/main/java/smu/FittingPair/jwt/JWTProvider.java
+++ FittingPair/src/main/java/smu/FittingPair/jwt/JWTProvider.java
@@ -1,6 +1,7 @@
 package smu.FittingPair.jwt;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -46,9 +47,10 @@ public class JWTProvider {
     public String getRole(String token){
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
     }
-    public Long getExpiredIn(String token){
+    public Long getExpiredIn(String token) {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().getTime();
     }
+
     public Authentication getAuthentication(String token) {
         UserDetails userDetails = userDetailsService.loadUserByUsername(getUserId(token));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
@@ -57,7 +59,8 @@ public class JWTProvider {
     //토큰 생성
     public String createAccessToken(String username){
         //30분
-        long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
+        //long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
+        long ACCESS_TOKEN_EXPIRE_TIME = 1000*15;
         return Jwts.builder()
                 .claim("id",username)
                 .claim("role","ROLE_USER") //Long id
@@ -68,7 +71,8 @@ public class JWTProvider {
     }
     public String createRefreshToken(String username){
         //7일
-        long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;
+        //long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;
+        long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60;
         return Jwts.builder()
                 .claim("id",username)
                 .claim("role","ROLE_USER")
