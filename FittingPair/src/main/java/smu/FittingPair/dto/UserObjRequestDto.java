2dcd243089d1efc986d3057ceb891f19e9fe8cf3
chaerish <ccyy8432@naver.com>
2024-08-18 04:22:56 +0900

    fix: 비동기 이미지 전송 처리

--- FittingPair/src/main/java/smu/FittingPair/dto/UserObjRequestDto.java
+++ FittingPair/src/main/java/smu/FittingPair/dto/UserObjRequestDto.java
@@ -1,6 +1,7 @@
 package smu.FittingPair.dto;
 
 import lombok.*;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
@@ -12,8 +13,8 @@ public class UserObjRequestDto {
     Long id; //userImgId
     MultipartFile multipartFile; //userImg
 
-    public static UserObjRequestDto to(Long id, MultipartFile multipartFile){
-       return UserObjRequestDto.builder().id(id).multipartFile(multipartFile).build();
+    public static UserObjRequestDto to(Long id, byte [] fileBytes){
+       return UserObjRequestDto.builder().id(id).build();
     }
 
 }
