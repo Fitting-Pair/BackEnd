174119442823078539a27a52f55bef21b95d465b
kimsj0970 <kimsj0970@naver.com>
2024-08-27 00:02:33 +0900

    fix: error fix

--- FittingPair/src/main/java/smu/FittingPair/dto/MyPageEditDto.java
+++ FittingPair/src/main/java/smu/FittingPair/dto/MyPageEditDto.java
@@ -3,11 +3,6 @@ package smu.FittingPair.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
-<<<<<<< HEAD
-import java.util.Optional;
-
-=======
->>>>>>> 184eab97446d299801b90f8a6efb8996b133b7fe
 @Getter
 @NoArgsConstructor
 @Builder
@@ -16,11 +11,6 @@ import java.util.Optional;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class MyPageEditDto {
     private String userName;
-<<<<<<< HEAD
-=======
-    private String phoneNumber;
-    private String gender;
->>>>>>> 184eab97446d299801b90f8a6efb8996b133b7fe
     private Long height;
 
 }
